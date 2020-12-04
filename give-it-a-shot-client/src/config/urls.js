86acb3b88846e0cc12cd0d16ac3815755ab61eff
Derek Foster <derekfoster94@gmail.com>
2020-12-03 16:07:26 -0800

    env var for backend url

--- give-it-a-shot-client/src/config/urls.js
+++ give-it-a-shot-client/src/config/urls.js
@@ -1,3 +1,5 @@
-const REACT_APP_API_URL = "http://localhost:5000/api/v1";
+require("dotenv").config();
+const REACT_APP_API_URL =
+  proccess.env.GIVE_IT_A_SHOT_URL || "http://localhost:5000/api/v1";
 
 export default REACT_APP_API_URL;
