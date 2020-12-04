d3178062a2ebef9e11a293b59970b6d58ef5fad4
Derek Foster <derekfoster94@gmail.com>
2020-12-04 15:18:54 -0800

    changed back

--- give-it-a-shot-api-sql/routes/auth.js
+++ give-it-a-shot-api-sql/routes/auth.js
@@ -3,7 +3,7 @@ const passport = require("../passport");
 const ctrl = require("../controllers");
 
 // PATH = /api/v1/auth
-// , { session: false } after local, tried with jacob
+//  after local, tried with jacob
 router.post("/login", passport.authenticate("local"), ctrl.auth.login);
 router.post("/register", ctrl.auth.register);
 router.delete("/logout", ctrl.auth.logout);
